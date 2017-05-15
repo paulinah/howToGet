ec8e53cfc072962ce134550b7a7de3e3c7a9b2a1
PNakraszewicz <pawel.nakraszewicz@gmail.com>
2017-05-15 13:52:48 +0200

    savePosition

--- app/src/main/java/com/example/user/myapplication/SavePosition.java
+++ app/src/main/java/com/example/user/myapplication/SavePosition.java
@@ -1,23 +1,52 @@
 package com.example.user.myapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Paula on 2017-05-08.
  */
 
-public class SavePosition extends AppCompatActivity {
+public class SavePosition extends Activity {
+
+    GPSTracker gps;
+    Button btnShowLocation;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.save_my_position);
 
+        btnShowLocation = (Button) findViewById(R.id.save_my_position);
 
-        getSupportActionBar().hide(); //hide ActionBar
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(SavePosition.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
     }
-
-
 }
